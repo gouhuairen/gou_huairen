da744e1185d9f43fa2761cf569aed4d160f46233
apple <372688118@qq.com>
2018-11-20 17:06:28 +0800

    第四章作业

---
+++ 04_20.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+   int passes = 0;
+   int failures = 0;
+   int studentcounter = 1;
+   int result;
+  
+   while ( studentcounter <= 10 )
+   {
+      cout << "Enter result (1 = pass, 2 = fail): ";
+      cin >> result;
+
+      if ( result == 1 )
+      {
+         passes = passes + 1;
+         studentcounter = studentcounter + 1;
+      }
+      else if ( result == 2 )
+      {
+         failures = failures + 1;
+         studentcounter = studentcounter + 1;
+      }
+      else
+      {
+         cout << "Invalid input" << endl;
+      }
+   }
+   cout << "Passed " << passes << "\nFailed " << failures << endl;
+
+   if ( passes > 8 )
+      cout << "Bonus to instructor!" << endl;
+}
