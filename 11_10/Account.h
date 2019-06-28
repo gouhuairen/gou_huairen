eb3682b4e00c258af66ab0f7ccb3aeb68a3a47d5
apple <372688118@qq.com>
2019-06-28 12:26:52 +0800

    第十一章  测绘工程17-4  苟怀仁

---
+++ 11_10/Account.h
@@ -0,0 +1,16 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+class Account
+{
+public:
+   Account( double );
+   void credit( double );
+   bool debit( double );
+   void setBalance( double );
+   double getBalance();
+private:
+   double balance;
+};
+
+#endif
